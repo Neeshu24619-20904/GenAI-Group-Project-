8ceb31ef463a06e203de1f126a650495d880b96a
Neeshu24619-20904 <neeshant241692@gmail.com>
2026-06-21 00:39:03 +0530

    Fixing bugs

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gen-ai-group-project.vercel.app/"],
+    allow_origins=["https://gen-ai-group-project.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
